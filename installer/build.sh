9327b5a55091c63f4fc972939c83cdf0186550eb
Rodolphe Pineau <pineau@rti-zone.org>
2020-03-19 14:43:03 -0700

    First import of the pre-release version

---
+++ installer/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+PACKAGE_NAME="SmartFocus_X2.pkg"
+BUNDLE_NAME="org.rti-zone.SmartFocusX2"
+
+if [ ! -z "$app_id_signature" ]; then
+    codesign -f -s "$app_id_signature" --verbose ../build/Release/libSmartFocus.dylib
+fi
+
+mkdir -p ROOT/tmp/SmartFocus_X2/
+cp "../SmartFocus.ui" ROOT/tmp/SmartFocus_X2/
+cp "../focuserlist SmartFocus.txt" ROOT/tmp/SmartFocus_X2/
+cp "../build/Release/libSmartFocus.dylib" ROOT/tmp/SmartFocus_X2/
+
+
+if [ ! -z "$installer_signature" ]; then
+	# signed package using env variable installer_signature
+	pkgbuild --root ROOT --identifier $BUNDLE_NAME --sign "$installer_signature" --scripts Scripts --version 1.0 $PACKAGE_NAME
+	pkgutil --check-signature ./${PACKAGE_NAME}
+
+else
+    pkgbuild --root ROOT --identifier $BUNDLE_NAME --scripts Scripts --version 1.0 $PACKAGE_NAME
+fi
+
+rm -rf ROOT
